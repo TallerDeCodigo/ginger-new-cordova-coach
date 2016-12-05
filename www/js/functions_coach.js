20f1bf38cccfc8762c23834886513f3cedc89be4
John Falcon <john@pixel-muffin.com>
2016-12-05 01:50:43 -0600

    Create ingredient scrollable

--- www/js/functions_coach.js
+++ www/js/functions_coach.js
@@ -6,16 +6,6 @@
 
 	$(function(){
 
-		$(".acc-selector").click(function(){
-			if ($(this).hasClass('ui-state-active')) {
-				if ($(this).hasClass('desayuno')) {$(this).parent().parent().animate({scrollTop:0}, 300);}
-				if ($(this).hasClass('snack1')) {$(this).parent().parent().animate({scrollTop:54}, 300);}
-				if ($(this).hasClass('comida')) {$(this).parent().parent().animate({scrollTop:120}, 300);}
-				if ($(this).hasClass('snack2')) {$(this).parent().parent().animate({scrollTop:184}, 300);}
-				if ($(this).hasClass('cena')) {$(this).parent().parent().animate({scrollTop:248}, 300);}
-			}
-		});
-
 		// Seguro desea cambiar la dieta?
 		$('.di-options a').click(function() {
 			$('.overscreen2').removeClass('active');
--- www/js/scripts/document_ready.js
+++ www/js/scripts/document_ready.js
@@ -799,6 +799,16 @@ window.initializeEvents = function(){
 				$('#blur').toggleClass('blurred');
 			});
 
+			$(".acc-selector").click(function(){
+				if ($(this).hasClass('ui-state-active')) {
+					if ($(this).hasClass('desayuno')) {$(this).parent().parent().animate({scrollTop:0}, 300);}
+					if ($(this).hasClass('snack1')) {$(this).parent().parent().animate({scrollTop:54}, 300);}
+					if ($(this).hasClass('comida')) {$(this).parent().parent().animate({scrollTop:120}, 300);}
+					if ($(this).hasClass('snack2')) {$(this).parent().parent().animate({scrollTop:184}, 300);}
+					if ($(this).hasClass('cena')) {$(this).parent().parent().animate({scrollTop:248}, 300);}
+				}
+			});
+
 
 			/*
 				CREATING A NEW DIET
